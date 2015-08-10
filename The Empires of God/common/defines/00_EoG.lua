9473ca8ec098ce0dc9404db2af7098442e80253e
fanoI <fanoIlPrimo@gmail.com>
2015-08-10 18:15:42 +0200

    0.0.3 Alpha
    - Compatible with "The Horse Lord DLC" and accompagning patches
    - RIRSEI events ID were no more OK because were > 99'999 changed them
    all!
    - Fixed RIRSEI decisions that referred to old laws

---
+++ The Empires of God/common/defines/00_EoG.lua
@@ -0,0 +1,6 @@
+-- Format for overwriting define values:
+--
+-- NDefines.NDiplomacy.DEMESNE_BASE_MAX_SIZE = 2.0
+
+NDefines.NTitle.MAX_REPUBLIC_COUNTIES_IN_REALM = 1.0		-- Removed this limitations (makes no sense)
+NDefines.NTitle.MAX_THEOCRACY_COUNTIES_IN_REALM = 1.0		-- Removed this limitations (makes no sense)
